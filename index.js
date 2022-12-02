aa2316e92c2f25686e456a375744365fa36a2734
d3v-tushar <d3v.tushar@gmail.com>
2022-12-02 08:51:53 +0600

    JWT Verification Added & User API Created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,11 +17,30 @@ app.use(express.json());
 //MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@learnph.159fxoq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const verifyJWT = (req, res, next) =>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) =>{
+        if(error){
+            return res.status(403).send({message: 'Forbidden Access'})
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
+
 const run = async() =>{
     try{
         const categoriesCollection = client.db("phoneSeeker").collection("categories");
         const phonesCollection = client.db("phoneSeeker").collection("phones");
         const bookingCollection = client.db("phoneSeeker").collection("bookings");
+        const usersCollection = client.db("phoneSeeker").collection("users");
+
         app.get('/categories', async(req, res)=>{
             const query = {};
             const cursor = categoriesCollection.find(query);
@@ -42,12 +62,29 @@ const run = async() =>{
             res.send(result);
         });
 
-        app.get('/bookings', async(req, res) =>{
+        app.get('/bookings', verifyJWT, async(req, res) =>{
             const email = req.query.email;
             const query = {email: email};
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
-        })
+        });
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.get('/jwt', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+                return res.send({accessToken: token})
+            }
+            res.status(403).send({message: 'Forbidden Access'});
+        });
 
         // app.get('/phones', async(req, res)=>{
         //     const query = {};
